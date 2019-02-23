9e11a102922e8d023283585fa254584960cc39c8
highslater <highslater@hotmail.com.com>
2019-02-22 20:26:58 -0500

    commit

--- 01a_Router_Default.py
+++ 01a_Router_Default.py
@@ -1,49 +1,55 @@
 #!/usr/bin/env python3
 
-##01_Router_Default
+##01a_Router_Default
 
 
 import getpass
 import telnetlib
 
-HOST = input("Enter HOST ADDRESS: ")
 user = input("Enter your remote account: ")
 password = getpass.getpass()
 
-tn = telnetlib.Telnet(HOST)
-tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
+f= open("Routers.txt")
 
-if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
+for line in f:
+    tn = telnetlib.Telnet(line)
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
 
-tn.write(b"conf t\n")
-tn.write(b"service password-encryption\n")
-tn.write(b"enable secret cisco\n")
-tn.write(b"username ccna privilege 15 secret cisco\n")
-tn.write(b"ip domain-lookup\n")
-tn.write(b"ip name-server 192.168.1.1\n")
-tn.write(b"ip http server\n")
-tn.write(b"ip domain-name gns3.com\n")
-tn.write(b"line console 0\n")
-tn.write(b"logging synchronous\n")
-tn.write(b"exec-timeout 0 0\n")
-tn.write(b"login local\n")
-tn.write(b"line vty 0 4\n")
-tn.write(b"logging synchronous\n")
-tn.write(b"exec-timeout 0 0\n")
-tn.write(b"login local\n")
-tn.write(b"line aux 0\n")
-tn.write(b"logging synchronous\n")
-tn.write(b"exec-timeout 0 0\n")
-tn.write(b"login local\n")
-tn.write(b"exit\n")
-tn.write(b"exit\n")
-tn.write(b"cop r s\n")
-tn.write(b"\n")
-tn.write(b"exit\n")
-print("\n\n\n*** START ***")
-print(tn.read_all().decode('ascii'))
-print("*** COMPLETE ***\n\n\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+
+    tn.write(b"conf t\n")
+    tn.write(b"service password-encryption\n")
+    tn.write(b"enable secret cisco\n")
+    tn.write(b"username ccna privilege 15 secret cisco\n")
+    tn.write(b"ip domain-lookup\n")
+    tn.write(b"ip name-server 192.168.1.1\n")
+    tn.write(b"ip http server\n")
+    tn.write(b"ip domain-name gns3.com\n")
+    tn.write(b"line console 0\n")
+    tn.write(b"logging synchronous\n")
+    tn.write(b"exec-timeout 0 0\n")
+    tn.write(b"login local\n")
+    tn.write(b"line vty 0 4\n")
+    tn.write(b"logging synchronous\n")
+    tn.write(b"exec-timeout 0 0\n")
+    tn.write(b"login local\n")
+    tn.write(b"line aux 0\n")
+    tn.write(b"logging synchronous\n")
+    tn.write(b"exec-timeout 0 0\n")
+    tn.write(b"login local\n")
+    tn.write(b"exit\n")
+    tn.write(b"exit\n")
+    tn.write(b"cop r s\n")
+    tn.write(b"\n")
+    tn.write(b"exit\n")
+
+#####Output: >>>>> Commands Entered
+    print("\n***** START *****")
+    print("*****" + line)
+    print(tn.read_all().decode('ascii'))
+    print("***** COMPLETE *****")
+    print("*****" + line)
 
