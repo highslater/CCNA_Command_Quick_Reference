59248813193d21c75a1d754121d443ed26ec8eba
highslater <highslater@hotmail.com.com>
2019-02-26 14:11:24 -0500

    commit

--- SSH_Configs/00n_SSH_netmiko.py
+++ SSH_Configs/00n_SSH_netmiko.py
@@ -6,32 +6,28 @@
 
 
 from netmiko import ConnectHandler
+import time
 
-R1 = {
-    'device_type': 'cisco_ios',
-    'ip': '10.0.0.1',
-    'username': 'ccna',
-    'password': 'cisco',
-}
+file = open('rt_test_list.txt')
 
-print("\n\n****** SSH : user = (" + R1['username'] + ")")
-print("CONNECTING to >  " + R1['ip'])
-
-net_connect = ConnectHandler(**R1)
-
-print("\t      >   CONNECTED\n")
-
-output_0 = net_connect.send_command('show ip route | i via')
-output_1 = net_connect.send_command('show ip interface brief | include up')
-output_2 = net_connect.send_command('show run | s eigrp')
-output_3 = net_connect.send_command('show run | s ip access-list')
-
-print("\nOUTPUT BEGINS >\n\n")
-print(output_0 + "\n\n")
-print(output_2 + "\n\n")
-print(output_3 + "\n\n")
-print(output_1 + "\n")
-print("\n< OUTPUT ENDS")
-print("\n\n********* SSH : user = (" + R1['username'] + ")")
-print("DISCONNECTING from <  " + R1['ip'])
-print("\t\t   <   DISCONNECTED\n\n")
+for line in file:
+    D = eval(line)[1]
+    R = eval(line)[0]
+    print("\n\n****** SSH : user = (" + D['username'] + ")")
+    print("CONNECTING to :  " + R + "  <  at  >  " +  D['ip'])
+    net_connect = ConnectHandler(**D)
+    print("\t      >   CONNECTED\n")
+    output_0 = net_connect.send_command('show ip route | i via')
+    output_1 = net_connect.send_command('show ip interface brief | include up')
+    output_2 = net_connect.send_command('show run | s eigrp')
+    output_3 = net_connect.send_command('show run | s ip access-list')
+    print("\nOUTPUT BEGINS >\n\n")
+#    print(output_0 + "\n\n")
+#    print(output_2 + "\n\n")
+#    print(output_3 + "\n\n")
+    print(output_1 + "\n")
+    print("\n< OUTPUT ENDS")
+    print("\n\n********* SSH : user = (" + D['username'] + ")")
+    print("DISCONNECTING from :  " + R + "  <  at  >  " + D['ip'])
+#    time.sleep(2)
+    print("\t\t   <   DISCONNECTED\n\n")
