db00d305224aa298e4fb103d6032e2068ecf6e94
highslater <highslater@hotmail.com.com>
2019-02-27 12:49:38 -0500

    commit

--- SSH_Configs/00n_SSH_netmiko.py
+++ SSH_Configs/00n_SSH_netmiko.py
@@ -8,26 +8,28 @@
 from netmiko import ConnectHandler
 import time
 
-file = open('rt_test_list.txt')
+f1 = open('Routers.txt')
+f2 = open('Switches.txt')
 
-for line in file:
-    D = eval(line)[1]
-    R = eval(line)[0]
-    print("\n\n****** SSH : user = (" + D['username'] + ")")
-    print("CONNECTING to :  " + R + "  <  at  >  " +  D['ip'])
-    net_connect = ConnectHandler(**D)
-    print("\t      >   CONNECTED\n")
-    output_0 = net_connect.send_command('show ip route | i via')
-    output_1 = net_connect.send_command('show ip interface brief | include up')
-    output_2 = net_connect.send_command('show run | s eigrp')
-    output_3 = net_connect.send_command('show run | s ip access-list')
-    print("\nOUTPUT BEGINS >\n\n")
-#    print(output_0 + "\n\n")
-#    print(output_2 + "\n\n")
-#    print(output_3 + "\n\n")
-    print(output_1 + "\n")
-    print("\n< OUTPUT ENDS")
-    print("\n\n********* SSH : user = (" + D['username'] + ")")
-    print("DISCONNECTING from :  " + R + "  <  at  >  " + D['ip'])
-#    time.sleep(2)
-    print("\t\t   <   DISCONNECTED\n\n")
+for file in (f1, f2):
+    for line in file:
+        D = eval(line)[1]
+        R = eval(line)[0]
+        print("\n\n****** SSH : user = (" + D['username'] + ")")
+        print("CONNECTING to :  " + R + "  <  at  >  " +  D['ip'])
+        net_connect = ConnectHandler(**D)
+        print("\t      >   CONNECTED\n")
+        output_0 = net_connect.send_command('show ip route | i via')
+        output_1 = net_connect.send_command('show ip interface brief | include up')
+        output_2 = net_connect.send_command('show run | s eigrp')
+        output_3 = net_connect.send_command('show run | s ip access-list')
+        print("\nOUTPUT BEGINS >\n\n")
+#        print(output_0 + "\n\n")
+#        print(output_2 + "\n\n")
+#        print(output_3 + "\n\n")
+        print(output_1 + "\n")
+        print("\n< OUTPUT ENDS")
+        print("\n\n********* SSH : user = (" + D['username'] + ")")
+        print("DISCONNECTING from :  " + R + "  <  at  >  " + D['ip'])
+#        time.sleep(2)
+        print("\t\t   <   DISCONNECTED\n\n")
